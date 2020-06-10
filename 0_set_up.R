be172b0cee54059f386e2e3c7b77d65c7ef2d026
Jay Ware <54444219+jayware9@users.noreply.github.com>
2020-06-10 12:36:15 +0100

    Simplified code for transposing annotations list
    Looks like purrr:transpose() isn't quite identical to the function I wrote, as it doesn't retain the structure exactly - in particular dates were being coerced to numeric.
    Though I've simplified the code a bit based on the discussion at https://github.com/tidyverse/purrr/issues/471

---
+++ 0_set_up.R
@@ -0,0 +1,219 @@
+# Load libraries ----------------------------------------------------------
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(here)
+library(sf)
+library(lubridate)
+library(plotly)
+
+# Define data paths -------------------------------------------------------
+datasets <- plots <- shapes <- list()
+
+data_path <- function(pattern) {
+  list.files(path = "data",
+             pattern = pattern) %>%
+    paste("data/", ., sep = "") %>%
+    here::here()
+}
+
+paths <- list(
+  sg = data_path("^Trends\\+in\\+daily\\+COVID-19\\+data\\+.*\\.xlsx"),
+  nrs = data_path("^covid-deaths-data-week-[0-9][0-9]\\.xlsx"),
+  phs = data_path("^202[0-9]-[0-9][0-9]-[0-9][0-9]-ed-weekly-nhsscotland-data.csv$"),
+  pop_est = here::here("data/mid-year-pop-est-19-info.xlsx"),
+  sitrep = here::here("data/Dashboard - Time series.xlsx"),
+  text = here::here("data/text.xlsx")
+)
+
+# Define plotting variables -----------------------------------------------
+col_palette <- c(sg_grey = "#8e979c", # From SG logo
+                 sg_blue = "#0065bd", # From SG logo
+                 sg_light_blue = "#E5F0F8") # From menu items on SG website
+hoverlabel_spark <- list(
+  bgcolor = col_palette[["sg_light_blue"]],
+  bordercolor = "white",
+  font = list(color = "black")
+)
+dates <- c(
+  lockdown = "2020-03-26",
+  WHO_declares_pandemic = "2020-03-11",
+  start_sparklines = "2020-03-07"
+)
+spark_height <- 50
+
+
+# Create shapes -----------------------------------------------------------
+shapes <- list(
+  "1r" = list(
+    list(
+      type = "line",
+      layer = "below",
+      x0 = "2020-02-19",
+      x1 = "2020-05-27",
+      y0 = 1,
+      y1 = 1,
+      line = list(color = "black", dash = "dot")
+    ),
+    list(
+      type = "line",
+      layer = "below",
+      x0 = "2020-03-26",
+      x1 = "2020-03-26",
+      y0 = 0,
+      y1 = 7,
+      line = list(color = col_palette["sg_grey"], dash = "dot")
+    )
+  ),
+  "1a" = list(
+    type = "line",
+    layer = "below",
+    x0 = dates[["lockdown"]],
+    x1 = dates[["lockdown"]],
+    y0 = 0,
+    y1 = 160,
+    line = list(color = col_palette["sg_grey"], dash = "dot")
+  ),
+  "1c" = list(
+    list(
+      type = "rect",
+      layer = "below",
+      fillcolor = col_palette[["sg_light_blue"]],
+      x0 = dates[["lockdown"]],
+      x1 = as.Date("2020-04-11"),
+      y0 = 0,
+      y1 = 1596,
+      line = list(width = 0)
+    )
+  ),
+  "2a" = list(
+    list(
+      type = "line",
+      layer = "below",
+      x0 = 13,
+      x1 = 13,
+      y0 = 3000,
+      y1 = 11059,
+      line = list(color = col_palette["sg_grey"], dash = "dot")
+    ),
+    list(
+      type = "line",
+      layer = "below",
+      x0 = 17,
+      x1 = 17,
+      y0 = 3000,
+      y1 = 15056,
+      line = list(color = col_palette["sg_grey"], dash = "dot")
+    )
+  ),
+  "3a" = list(
+    list(
+      type = "rect",
+      layer = "below",
+      fillcolor = col_palette[["sg_light_blue"]],
+      x0 = as.Date("2020-04-06"),
+      x1 = as.Date("2020-04-20"),
+      y0 = 0,
+      y1 = 2106,
+      line = list(width = 0)
+    )
+  ),
+  "4a" = list(
+    list(
+      type = "line",
+      layer = "below",
+      x0 = dates[["WHO_declares_pandemic"]],
+      x1 = dates[["WHO_declares_pandemic"]],
+      y0 = 0,
+      y1 = 5000,
+      line = list(color = col_palette["sg_grey"], dash = "dot")
+    ),
+    list(
+      type = "line",
+      layer = "below",
+      x0 = dates[["lockdown"]],
+      x1 = dates[["lockdown"]],
+      y0 = 0,
+      y1 = 9500,
+      line = list(color = col_palette["sg_grey"], dash = "dot")
+    )
+  )
+)
+
+
+# Define function(s) ------------------------------------------------------
+add_style_spark <- function(p) {
+  p %>% add_trace(
+    type = "scatter",
+    mode = "lines",
+    line = list(color = "black"),
+    fill = "tozeroy",
+    fillcolor = col_palette["sg_light_blue"],
+    hoverinfo = "text"
+  ) %>% 
+    config(displayModeBar = FALSE,
+           showAxisDragHandles = FALSE) %>% 
+    layout(
+      paper_bgcolor = "rgba(0, 0, 0, 0)",
+      plot_bgcolor = "rgba(0, 0, 0, 0)",
+      yaxis = list(
+        fixedrange = TRUE,
+        title = "",
+        showgrid = FALSE,
+        zeroline = FALSE,
+        rangemode = "tozero",
+        showticklabels = FALSE
+      ),
+      xaxis = list(
+        fixedrange = TRUE,
+        title = "",
+        showgrid = FALSE,
+        showticklabels = FALSE,
+        range = c(dates[["start_sparklines"]], as.character(Sys.Date()))
+      ),
+      margin = list(l = 0,
+                    r = 0,
+                    t = 0,
+                    b = 0)
+    ) %>% 
+    htmlwidgets::onRender(
+      "function(el, x) {
+      Plotly.d3.select('.cursor-pointer').style('cursor', 'crosshair')}"
+    )
+}
+
+add_style_chart <- function(p) {
+  p %>% config(displayModeBar = FALSE,
+               showAxisDragHandles = FALSE) %>%
+    layout(
+      xaxis = list(
+        linecolor = rgb(255, 255, 255, maxColorValue = 255),
+        width = 0,
+        fill = NA,
+        fixedrange = TRUE,
+        bty = "n",
+        showline = FALSE,
+        title = "",
+        showgrid = FALSE,
+        zeroline = FALSE
+      ),
+      yaxis = list(
+        fixedrange = TRUE,
+        showline = FALSE,
+        title = "",
+        showgrid = FALSE,
+        tick0 = 0,
+        zeroline = FALSE,
+        rangemode = "tozero",
+        tickformat = ","
+      ),
+      paper_bgcolor = "rgba(0, 0, 0, 0)",
+      plot_bgcolor = "rgba(0, 0, 0, 0)",
+      margin = list(l = 0,
+                    r = 0)
+    ) %>% 
+    htmlwidgets::onRender(
+      "function(el, x) {
+      Plotly.d3.select('.cursor-pointer').style('cursor', 'crosshair')}"
+    )
+}
---
+++ 2_plot_charts.R
@@ -0,0 +1,208 @@
+# Harms 1 ---------------------------------------------------------------------
+plots[["1r"]] <- plot_ly(
+  data = datasets[["1r"]],
+  x = ~ date,
+  hoverinfo = "text",
+  y = ~ middle
+) %>%
+  add_trace(
+    type = "scatter",
+    mode = "markers",
+    marker = list(opacity = 0),
+    error_y = ~list(array = high - middle,
+                    arrayminus = middle - low,
+                    color = col_palette["sg_blue"],
+                    thickness = 5,
+                    width = 6),
+    text = ~ text
+  ) %>% 
+  add_style_chart() %>%
+  layout(
+    xaxis = list(showspikes = TRUE,
+                 spikemode = "across"),
+    shapes = shapes[["1r"]],
+    annotations = filter(annotations, plot == "1r", dataset == "1r")
+  )
+
+plots[["1a"]] <- plot_ly(
+  data = datasets[["1a"]],
+  x = ~ Date,
+  y = ~ count,
+  hoverinfo = "text"
+) %>%
+  add_trace(
+    name = "Deaths",
+    y = ~ count,
+    type = "bar",
+    marker = list(color = col_palette["sg_grey"]),
+    text = ~ count_text
+  ) %>%
+  add_trace(
+    name = "7 day average",
+    y = ~ count_7day_avg,
+    type = "scatter",
+    mode = "markers+lines",
+    marker = list(size = 7, color = col_palette["sg_blue"]),
+    line = list(color = col_palette["sg_blue"]),
+    text = ~ count_7day_avg_text
+  ) %>%
+  add_style_chart() %>%
+  layout(
+    showlegend = FALSE,
+    shapes = list(
+      list(
+        type = "line",
+        layer = "below",
+        x0 = dates[["lockdown"]],
+        x1 = dates[["lockdown"]],
+        y0 = 0,
+        y1 = 160,
+        line = list(color = col_palette["sg_grey"], dash = "dot")
+      )
+    ),
+    annotations = filter(annotations, plot == "1a", dataset == "1a"),
+    legend = list(orientation = 'h',
+                  x = 0, y = 100)
+  )
+
+plots[["1b"]] <- plot_ly(
+  data = datasets[["1b"]],
+  x = ~ week_ending_date,
+  y = ~ deaths,
+  marker = list(size = 7),
+  name = ~ setting,
+  linetype = ~ linetype,
+  hoverinfo = "text",
+  text = ~ text
+) %>%
+  add_trace(
+    type = "scatter",
+    mode = "markers+lines"
+  ) %>%
+  config(displayModeBar = FALSE,
+         showAxisDragHandles = FALSE) %>%
+  layout(
+    showlegend = FALSE,
+    xaxis = list(
+      linecolor = rgb(255, 255, 255, maxColorValue = 255),
+      width = 0,
+      fill = NA,
+      fixedrange = TRUE,
+      bty = "n",
+      showline = FALSE,
+      title = "",
+      showgrid = FALSE,
+      zeroline = FALSE
+    ),
+    yaxis = list(
+      fixedrange = TRUE,
+      showline = FALSE,
+      title = "",
+      showgrid = FALSE,
+      tick0 = 0,
+      zeroline = FALSE
+    ),
+    colorway = c(col_palette[c("sg_grey", "sg_blue", "sg_blue", "sg_grey")]),
+    paper_bgcolor = "rgba(0, 0, 0, 0)",
+    plot_bgcolor = "rgba(0, 0, 0, 0)",
+    margin = list(l = 0,
+                  r = 0),
+    legend = list(orientation = 'h',
+                  x = 0, y = 100),
+    annotations = filter(annotations, plot == "1b", dataset == "1b")
+  ) %>% 
+  htmlwidgets::onRender(
+    "function(el, x) {
+      Plotly.d3.select('.cursor-pointer').style('cursor', 'crosshair')}"
+  )
+
+plots[["1c"]] <- plot_ly(
+  data = datasets[["1c"]],
+  x = ~ date,
+  y = ~ covid_patients,
+  marker = list(size = 7),
+  name = ~ location_label,
+  hoverinfo = ~ "text"
+) %>%
+  add_trace(type = "scatter",
+            mode = "markers+lines",
+            text = ~ text) %>%
+  add_style_chart() %>% 
+  layout(showlegend = FALSE,
+         colorway = c(col_palette),
+         legend = list(orientation = 'v',
+                       x = 0, y = 100),
+         annotations = filter(annotations, plot == "1c", dataset == "1c"),
+         shapes = shapes[["1c"]])
+  
+
+# Harms 2 ---------------------------------------------------------------------
+plots[["2a"]] <- plot_ly(
+  x = ~ lubridate::week(week_ending_date),
+  y = ~ attendance,
+  hoverinfo = ~ "text"
+) %>%
+  add_trace(data = datasets[["2a"]] %>% 
+              group_by(year = year(week_ending_date)) %>% 
+              filter(year != 2020),
+            type = "scatter",
+            mode = "lines",
+            text = ~ text) %>%
+  add_trace(data = datasets[["2a"]] %>% 
+              filter(year(week_ending_date) == 2020),
+            type = "scatter",
+            mode = "markers+lines",
+            marker = list(size = 7),
+            text = ~ text) %>% 
+  add_style_chart() %>% 
+  layout(
+    xaxis = list(title = "Week number"),
+    showlegend = FALSE,
+    colorway = c(col_palette),
+    shapes = shapes[["2a"]],
+    annotations = filter(annotations, plot == "2a", dataset == "2a") %>% 
+      mutate(x = week(x)) # Use week numbers instead of dates
+  )
+
+# Harms 3 ---------------------------------------------------------------------
+plots[["3a"]] <- plot_ly(
+  data = datasets[["3a"]],
+  x = ~ date,
+  y = ~ children,
+  marker = list(size = 7),
+  hoverinfo = ~ "text"
+) %>%
+  add_style_chart() %>%
+  add_trace(type = "scatter",
+            mode = "markers+lines",
+            text = ~ text) %>% 
+  layout(
+    shapes = shapes[["3a"]],
+    annotations = filter(annotations, plot == "3a", dataset == "3a")
+  )
+
+# Harms 4 ---------------------------------------------------------------------
+plots[["4a"]] <- plot_ly(
+  data = datasets[["4a"]],
+  x = ~ date,
+  marker = list(size = 7),
+  hoverinfo = ~ "text"
+) %>%
+  add_style_chart() %>%
+  add_trace(name = "Universal Credit Claims",
+            type = "bar",
+            y = ~ claims,
+            text = ~ claims_text) %>% 
+  add_trace(name = "7 day average",
+            type = "scatter",
+            mode = "markers+lines",
+            y = ~ claims_7day_avg,
+            text = ~ claims_7day_avg_text) %>% 
+  layout(
+    showlegend = FALSE,
+    colorway = c(col_palette),
+    legend = list(orientation = 'h',
+                  x = 0, y = 100),
+    shapes = shapes[["4a"]],
+    annotations = filter(annotations, plot == "4a", dataset == "4a")
+  )
